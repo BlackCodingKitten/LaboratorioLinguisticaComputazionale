8de6f8b1cb7c6f597b4102018fb900cd05bb5b0d
Michela <m.deodati@studenti.unipi.it>
2024-10-15 12:45:29 +0200

    fine dell'esercitazione, stampa con successo du un file.txt, ogni volta diverso perchè nel nome contiene la data. FINE

--- LCEsercitazioni/ES 14.10.2024/es1.py
+++ LCEsercitazioni/ES 14.10.2024/es1.py
@@ -17,8 +17,7 @@ BONUS:
 import nltk
 nltk.download('punkt_tab')
 import sys
-import json
-
+import datetime
 def read_file(file_path):
     """funzione che prende in input il contenuto testuale di un file e tramite l'impiego di nltk tokenizer restituisce la lista dei token
 
@@ -32,13 +31,13 @@ def read_file(file_path):
         file_pointer = open(file_path, "r")  
         return file_pointer.read() 
     except FileNotFoundError:
-        print("Il file "+file_path+" inserito non è stato trovato")
+        print(f"Il file "+file_path+" inserito non è stato trovato")
         return "-1"
     except PermissionError:
-        print("Non hai il permesso di aprire il file {file_path}")
+        print(f"Non hai il permesso di aprire il file {file_path}")
         return "-1"
     except IOError:
-        print("Errore generico di lettura del file {file_path}")
+        print(f"Errore generico di lettura del file {file_path}")
         return "-1"
 
 
@@ -61,53 +60,41 @@ def tokens_extractor(file_contents):
 
      
 def write_output(tokens_lists, sentences_lists, types_lists, files):
-    """funzione che scrive l'output dell'esecuzione su un file json
+    """funzione che scrive l'output dell'esecuzione su un file .txt
 
     Args:
-        tokens_lists (string list tuple): liste dei token del testo
-        sentences_lists (string list tuple): liste delle frasi del testo
-        types_lists (string list tuple): vocabolari dei testi
-        files (string list tuple): nomi dei file
+        tokens_lists (string list list): liste dei token del testo
+        sentences_lists (string list list): liste delle frasi del testo
+        types_lists (string list list): vocabolari dei testi
+        files (string list ): nomi dei file
     """   
     max_types_n= ("",0) 
     max_tokens_n=("",0)
     max_sentences_n=("",0) 
-    json_object_list=[]
     for i in range(0, len(files)):
-        for k in range (0, len(sentences_lists[i])):
-            sentences_lists[i][k]= str(i)+". "+sentences_lists[i][k]
-        print(sentences_lists[i][k])
-    #     json_object={
-    #         {files[i]}:{
-    #             "sentence list len":{len(sentences_lists[i])},
-    #             "token list len":{len(tokens_lists[i])},
-    #             "type list len":{len(types_lists[i])},
-    #             "sentences list": {sentences_lists[i]},
-    #             "token list": {tuple(sorted(tokens_lists[i]))},
-    #             "type list":{tuple(sorted(types_lists[i]))}
-    #         }
-    #     }
-    #     json_object_list.append(json.dumps(json_object, indent = 4))
-    #     #controllo il counter m,aggiore delle frasi
-    #     if(len(sentences_lists[i])>max_sentences_n):
-    #         max_sentences_n=(files[i],len(sentences_lists[i]))
-    #     #controllo il counter maggiore dei token
-    #     if(len(tokens_lists[i])>max_tokens_n):
-    #         max_tokens_n=(files[i],len(tokens_lists[i]))
-    #     #controlllo il counter maggiore dei tipi
-    #     if(len(types_lists[i])> max_types_n):
-    #          max_types_n=(files[i],len(types_lists[i]))
+        #controllo il counter maggiore delle frasi
+        if(len(sentences_lists[i])>max_sentences_n[1]):
+            max_sentences_n=(files[i],len(sentences_lists[i]))
+        #controllo il counter maggiore dei token
+        if(len(tokens_lists[i])>max_tokens_n[1]):
+            max_tokens_n=(files[i],len(tokens_lists[i]))
+        #controlllo il counter maggiore dei tipi
+        if(len(types_lists[i])> max_types_n[1]):
+             max_types_n=(files[i],len(types_lists[i]))
     
-    # result= {
-    #     "Max Sentences": "Il file con il numero maggiore di frasi e' {max_sentences_n[0]} e contiene {max_sentences_n[1]} frasi.",
-    #     "Max Tokens": "Il file con il numero maggiore di token e' {max_tokens_n[0]} e contiene {max_tokens_n[1]} frasi.",
-    #     "Max Type": "Il file con il numero maggiore di tipi e' {max_types_n[0]} e contiene {max_types_n[1]} frasi."
-    # }
-    # json_object_list.append(json.dumps(result, indent=4))
-    # with open('./results/results.json', "w") as results_file:
-    #     for json_object in json_object_list:
-    #         results_file.write(json_object)
-    #     results_file.close()
+    result="\n\nRISULTATO DEL CONFRONTO TRA FILE:\nMax Sentences:\tIl file con il numero maggiore di frasi e' "+(max_sentences_n[0].split("/"))[-1]+" e contiene "+str(max_sentences_n[1])+" frasi.\n"+"Max Tokens:\t"+"Il file con il numero maggiore di token e' "+(max_tokens_n[0].split("/"))[-1]+" e contiene "+str(max_tokens_n[1])+" token.\n"+"Max Type\t"+"Il file con il numero maggiore di tipi e' "+(max_types_n[0].split("/"))[-1]+" e contiene "+str(max_types_n[1])+" tipi.\n"
+    
+    with open("/home/mikela/Documents/LaboratoriLinguisticaComputazionale/LCEsercitazioni/ES 14.10.2024/results/results"+str(datetime.datetime.now())+".txt", "w") as results_file:
+        results_file.write("\n"+result+"\n")
+        for i in range(0,len(files)):
+            string_to_write=("NOME DEL FILE:\t"+files[i]+"\n")
+            string_to_write+="\nIl file contiene "+str(len(sentences_lists[i]))+" frasi, "+str(len(tokens_lists[i]))+" token e "+str(len(types_lists))+" tipi\n\n"
+            string_to_write+="-"*15+"LISTA DELLE FRASI:\n"+"\n".join(sentences_lists[i])+"\n"
+            string_to_write+="-"*15+"LISTA DEI TOKEN:\n"+"\n".join(sorted(tokens_lists[i]))+"\n"
+            string_to_write+="-"*15+"VOCABOLARIO DEI TIPI:\n"+"\n".join(sorted(types_lists[i]))+"\n"
+            string_to_write+="\n\n\n"
+            results_file.write(string_to_write)
+        results_file.close()
         
         
 def main(argv_list):
@@ -123,7 +110,7 @@ def main(argv_list):
             valid_file_name.append(path)
             file_list.append(file_text)
         else:
-            print("Impossibile leggere il file {path}")
+            print(f"Impossibile leggere il file {path}")
     if not file_list:
         print("Esecuzione terminata con errore.")
         return
@@ -143,5 +130,4 @@ def main(argv_list):
         
 if __name__ == "__main__":
     #prende solo i parametri effettivi (salta il nome del programma argv[0] e salta l'ultimo elemento che rappresenta la lunghezza di argv)
-    #main(sys.argv[1::(int(sys.argv[len(sys.argv)-1])-1)])
-    main(["/home/mikela/Documents/LaboratoriLinguisticaComputazionale/LCEsercitazioni/ES 14.10.2024/d1.txt","/home/mikela/Documents/LaboratoriLinguisticaComputazionale/LCEsercitazioni/ES 14.10.2024/d2.txt"])
+    main(sys.argv[1:])
