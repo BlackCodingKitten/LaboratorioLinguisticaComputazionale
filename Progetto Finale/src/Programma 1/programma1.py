ef60dab58e0f8f6e70ccefd30dc1b6e5ad741fe6
Michela <m.deodati@studenti.unipi.it>
2024-12-04 10:15:39 +0100

    creati i file .py  per programma 1 e programma 2

---
+++ Progetto Finale/src/Programma 1/programma1.py
@@ -0,0 +1,13 @@
+import sys
+
+
+def main(argv):
+    #safe exit se non è stato passato nessun file
+    if len(argv)<2:
+        print("Attenzione! Non hai passato nessun file da input.")
+        return
+    
+    return
+
+if __name__ == '__main__':
+    main(sys.argv)
